de1c49a41b821323642aadeafdf06fa15689b33d
Shelemhg <48326251+Shelemhg@users.noreply.github.com>
2023-12-12 22:47:11 -0600

    ci: Complete Frontend

--- app/layout.js
+++ app/layout.js
@@ -15,8 +15,8 @@ export default function RootLayout({ children }) {
 		<html lang="en">
 		<body className={inter.className}>
 			<div className="max-w-3xl mx-auto p-4">
-			<Navbar/>
-			{children}
+				<Navbar />
+				<div className="mt-8">{children}</div>
 			</div>
 			</body>
 		</html>
